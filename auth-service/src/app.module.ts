07e4455ff56f69c18d943b823ce1f3110eb62f00
puahuhu <nguyenletuanphuongzzz@gmail.com>
2025-09-12 19:53:40 +0700

    add table student

--- auth-service/src/app.module.ts
+++ auth-service/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'prisma/prisma.module';
-import { AuthModule } from 'src/modules/auth.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [PrismaModule, AuthModule],
--- auth-service/src/modules/auth/auth.service.ts
+++ auth-service/src/modules/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../../prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
